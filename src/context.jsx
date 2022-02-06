a08bf4e741dabdb18df85a4aae8c4e8ace089c98
faith okpala <okpalafaith1@gmail.com>
2022-02-06 18:56:59 +0100

    loading done

--- src/context.jsx
+++ src/context.jsx
@@ -11,12 +11,38 @@ import reducer from './reducer'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
-const initialState = {}
+const initialState = {
+  isLoading: true,
+  hits: [],
+  query: 'react',
+  page: 0,
+  nbPages: 0,
+}
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING })
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchStories
+  },[])
+
+  return <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
--- src/reducer.jsx
+++ src/reducer.jsx
@@ -6,5 +6,20 @@ import {
   HANDLE_SEARCH,
 } from './actions'
 
-const reducer = () => {}
+const reducer = (state, action) => {
+switch(action.type){
+  case SET_LOADING:
+    return{...state, isLoading: true}
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPages,
+      }
+    default:
+      throw new Error(`no mathching "${action.type}" action type`)
+  
+}
+}
 export default reducer
